ef3959ee0d7533f711a8ffd369b16c79721de55d
Izzan Silmi Aziz <izzan.abad@gmail.com>
2022-06-28 11:05:09 +0700

    belajar nodejs logging

---
+++ test/logger-console-transport.test.js
@@ -0,0 +1,14 @@
+import winston from "winston"
+
+test("create new logger with console transport", () => {
+    const logger = winston.createLogger({
+        transports: [
+            new winston.transports.Console({})
+        ]
+    });
+
+    logger.log({
+        level: "info",
+        message: "Hello logging"
+    })
+})
